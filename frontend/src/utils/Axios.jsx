bc7872f5be1fa8fff5c7f797dddd76b1254cbf6e
Zaid Yargatti <9928zaid@gmail.com>
2025-12-18 16:20:13 +0530

    Update Axios.jsx

--- frontend/src/utils/Axios.jsx
+++ frontend/src/utils/Axios.jsx
@@ -2,10 +2,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:  "http://localhost:3000", 
+  baseURL:  "https://full-stack-task-tracker-tdxc.onrender.com"|| "http://localhost:3000", 
   withCredentials:true
 });
-//"https://full-stack-task-tracker-tdxc.onrender.com" 
+ 
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
